0971a009ce6b0d87f2ebd66f7cd67864eb1df72f
mehedi <mehedi25122003@gmail.com>
2025-05-02 20:14:09 +0600

    react icon i

--- src/Components/Chatsection.jsx
+++ src/Components/Chatsection.jsx
@@ -1,13 +1,49 @@
 const Chatsection = () => {
-    return (
-        <section className="bg-gradient-to-r from-bg to-toBg  min-h-screen ">
-            
-            <div className="w-11/12 mx-auto">
-            <h1>Live Ai Coach</h1>
-            <p>Take a Suggestion Coaching </p>
+  return (
+    <section className="bg-gradient-to-r from-bg to-toBg  min-h-screen ">
+      <div className="w-11/12 mx-auto">
+        <h1>Live Ai Coach</h1>
+        <p>Take a Suggestion Coaching </p>
+
+        <div>
+          <div className="chat chat-start">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS chat bubble component"
+                  src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                />
+              </div>
             </div>
-        </section>
-    );
+            <div className="chat-header">
+              Obi-Wan Kenobi
+              <time className="text-xs opacity-50">12:45</time>
+            </div>
+            <div className="chat-bubble">You were the Chosen One!</div>
+            <div className="chat-footer opacity-50">Delivered</div>
+          </div>
+          <div className="chat chat-end">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS chat bubble component"
+                  src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+                />
+              </div>
+            </div>
+            <div className="chat-header">
+              Anakin
+              <time className="text-xs opacity-50">12:46</time>
+            </div>
+            <div className="chat-bubble">I hate you!</div>
+            <div className="chat-footer opacity-50">Seen at 12:46</div>
+          </div>
+          <div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Chatsection;
